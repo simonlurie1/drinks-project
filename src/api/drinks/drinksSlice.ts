8cedd77bcd0cbc481a32ace6f5809d1ad79dfc81
simonl <simonlurie1@gmail.com>
2025-03-15 20:48:28 +0200

    fixes

---
+++ src/api/drinks/drinksSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { CockTailState } from './types';
+
+const initialState: CockTailState = {
+  selectedDrinkId: null,
+};
+
+const drinksSlice = createSlice({
+  name: 'drinks',
+  initialState,
+  reducers: {
+    setSelectedDrink: (state, action: PayloadAction<number | null>) => {
+      state.selectedDrinkId = action.payload;
+    },
+  },
+});
+
+export const { setSelectedDrink } = drinksSlice.actions;
+export default drinksSlice.reducer;
---
+++ src/api/mocks/types.ts
@@ -0,0 +1,13 @@
+export interface MockType {
+  id?: string;
+  createdAt: string;
+  name: string;
+  image: string;
+  category: string;
+  alcoholic: string;
+  instructions: string;
+}
+
+export interface MockState {
+  id: number | null;
+}
